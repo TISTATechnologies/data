bc43fbe7440021f7f3a4b36d5fde17cbfae42e54
Evgen Rusakov <erusakov@data.tistatech.com>
2020-05-05 17:34:25 -0400

    Add index.html file for the root of the data.tistactech.com

--- scripts/devops/build-common-data.sh
+++ scripts/devops/build-common-data.sh
@@ -11,10 +11,10 @@ output_dir=${PWD}/build/common/v${version}
 title "Building '${output_dir}' directory..."
 rm -rf "${output_dir}"
 mkdir -p "${output_dir}" 2>/dev/null
-cp -vR "${data_dir}/common/" "${output_dir}"
+cp -vaT "${data_dir}/common" "${output_dir}"
 title "US zips files"
-./scripts/converter/create-us-zips-cache-directory.py "${output_dir}/us/zip"
+#./scripts/converter/create-us-zips-cache-directory.py "${output_dir}/us/zip"
 title "Create json format for all csv files"
-MINIFY=true ./scripts/converter/csv-to-json.sh "${output_dir}"
+#MINIFY=true ./scripts/converter/csv-to-json.sh "${output_dir}"
 title "Build 'common' directory complete: ${output_dir}"
 
